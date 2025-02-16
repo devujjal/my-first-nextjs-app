015110958f094e2a7f3dfffd7bc58a1f4a28facf
Developer Ujjal <ujjalchandra30@gmail.com>
2025-02-16 20:29:48 +0600

    understand the files structure and add nav and footer

---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+
+const Heading = () => {
+    return (
+        <div>
+            This is mine last heading for this page
+        </div>
+    );
+};
+
+export default Heading;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+    return (
+        <div>
+            <ul className="flex justify-center gap-5 py-3">
+                <li>Home</li>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+};
+
+export default NavBar;
